16ad9a6226f854ed70f9a654e8f56d150fe570fe
Cyanoxide <cyanoxide@gmail.com>
2025-02-25 16:54:04 +0000

    Apply Tailwind CSS where possible to inital layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Hand from './components/Hand/Hand';
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <Hand className="order-1 flex justify-center items-center"></Hand>
-      <Hand className="order-3 flex justify-center items-center"></Hand>
-      <Board className="order-2 justify-center items-center"></Board>
+      <Hand className="order-1 flex justify-center items-center gap-4 flex-col w-[20%]"></Hand>
+      <Hand className="order-3 flex justify-center items-center gap-4 flex-col w-[20%]"></Hand>
+      <Board className="order-2 grid justify-center items-center gap-4 w-[60%] m-auto"></Board>
     </div>
   );
 }
