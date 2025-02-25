30e02bd099a83796c26537be25996f0fcebc9813
Cyanoxide <cyanoxide@gmail.com>
2025-02-25 19:10:05 +0000

    Add basic card visuals

--- src/app/components/Hand/Hand.tsx
+++ src/app/components/Hand/Hand.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Hand.module.scss';
+import Card from '../Card/Card';
+
 
 interface HandProps {
     className?: string;
@@ -10,7 +12,7 @@ const Hand: React.FC<HandProps> = ({ className }) => {
         <div className={`${styles.hand} ${className || ''}`.trim()}>
             <div className="item"><div className="content">1</div></div>
             <div className="item"><div className="content">2</div></div>
-            <div className="item"><div className="content">3</div></div>
+            <div className="item"><div className="content"><Card id={2} top={9} right={9} bottom={9} left={9} image="" /></div></div>
             <div className="item"><div className="content">4</div></div>
             <div className="item"><div className="content">5</div></div>
         </div>
