2a9ac47d182ff7fc5eae599a03a4fc5dfb44158b
Cyanoxide <cyanoxide@gmail.com>
2025-02-26 16:58:05 +0000

    Add basic card placement with `placeCard`

--- src/app/components/Board/Board.tsx
+++ src/app/components/Board/Board.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState, useEffect } from 'react';
 import React from 'react';
 import styles from './Board.module.scss';
 import Card from '../Card/Card';
@@ -7,17 +10,34 @@ interface BoardProps {
 }
 
 const Board: React.FC<BoardProps> = ({ className }) => {
+    const [board, setBoard] = useState([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]);
+
+    const placeCard = (row: number, col: number, cardId: number, player: "red" | "blue") => {
+        setBoard(prevBoard => {
+            const newBoard = prevBoard.map(row => [...row]);
+            console.log(newBoard);
+            newBoard[row][col] = [cardId, player];
+            return newBoard;
+        });
+    };
+
+    useEffect(() => {
+        placeCard(1, 2, 14, "blue");
+    }, [setBoard]);
+
     return (
         <div className={`${styles.board} ${className || ''}`.trim()}>
-            <div className="cell"></div>
-            <div className="cell"><Card id={11} player="red" /></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"><Card id={2} player="blue" /></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"><Card id={6} player="red" /></div>
+            {board.map((row, rowIndex) => (
+                row.map((col, colIndex) => (
+                    <div key={`${rowIndex}-${colIndex}`} className="cell" data-position={[rowIndex, colIndex]}>
+                        {col && <Card id={col[0]} player={col[1]} />}
+                    </div>
+                ))
+            ))}
         </div >
     );
 };
