c19cd47803dedb6e6e210e66a40a04a769e2f632
Cyanoxide <cyanoxide@gmail.com>
2025-03-29 23:50:08 +0000

    Add blue and yellow font colour options

--- src/app/utils/textToSprite.tsx
+++ src/app/utils/textToSprite.tsx
@@ -1,8 +1,8 @@
-const textToSprite = (text: string) => {
+const textToSprite = (text: string, colour: string = "white") => {
     if (!text) return;
 
     return (
-        <span className="font">
+        <span className={`font ${colour}`}>
             {text.split("").map((glyph: string, index: number) => (
                 <span key={index} className="font-glyph" data-sprite={glyph}>{glyph}</span>
             ))}
