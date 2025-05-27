06940f43d596a2bf8a9cc2715c6a6df25d491b86
Cyanoxide <cyanoxide@gmail.com>
2025-05-27 09:02:59 +0100

    Separate card values into separate component

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -3,34 +3,29 @@ import styles from './Card.module.scss';
 import cards from '../../../data/cards.json';
 import { useGameContext } from "../../context/GameContext";
 import { PlayerType } from "../../context/GameTypes";
+import CardValues from "../../components/CardValues/CardValues";
 
 interface CardProps {
     id: number;
     player: PlayerType;
     onBoard?: boolean;
+    displayValues?: boolean;
     onClick?: () => void;
     onMouseEnter?: () => void;
     onMouseLeave?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ id, player, onBoard, ...props }) => {
-    const { isGameActive, rules, winState } = useGameContext();
+const Card: React.FC<CardProps> = ({ id, player, onBoard, displayValues = true, ...props }) => {
+    const { isGameActive, rules, winState, isCardGalleryOpen } = useGameContext();
     const card = cards.find(card => card.id === id);
-
-    const renderCardValue = (number: number) => (number === 10) ? "A" : number;
 
     if (!card) return;
 
     return (
         <div className={`${styles.card} ${(player === "red" && !winState && !onBoard && (!isGameActive || !rules?.includes("open"))) ? styles["card--hidden"] : ""} card relative`} data-player={player} {...props} >
-            <div className={styles.card__front} data-card-id={card.id} data-level={card.level}>
-                <div className={`${styles.values} relative`}>
-                    <span className={`${styles.topValue} absolute text-center`} data-sprite={card.top}>{renderCardValue(card.top)}</span>
-                    <span className={`${styles.rightValue} rightValue absolute text-center`} data-sprite={card.right}>{renderCardValue(card.right)}</span>
-                    <span className={`${styles.bottomValue} bottomValue absolute text-center`} data-sprite={card.bottom}>{renderCardValue(card.bottom)}</span>
-                    <span className={`${styles.leftValue} leftValue absolute text-center`} data-sprite={card.left}>{renderCardValue(card.left)}</span>
-                </div>
-                {card.element && <div className={`${styles.element} relative`} data-sprite={card.element}>{card.element}</div>}
+            <div className={styles.card__front} data-card-id={card.id} data-level={card.level} data-gallery={(isCardGalleryOpen) ? true : false}>
+                {displayValues && <CardValues cardId={id} />}
+                {displayValues && card.element && <div className={`${styles.element} relative`} data-sprite={card.element}>{card.element}</div>}
             </div>
         </div>
     );
